baa08dcdada6e76bf26b7e2ba0327bf25db07835
KenyaSogo <kenyasogo.dev@gmail.com>
2021-09-26 02:27:34 +0900

    fix: execute escalation controller: add logging

--- controller.js
+++ controller.js
@@ -1,6 +1,7 @@
 /** @OnlyCurrentDoc */
 
 function executeControl(){
+  console.log("start executeControl");
   // exec group の trigger 判定用の現在時刻セルに値をセットする
   const currentTime = getCurrent_yyyy_mm_dd_hh_mm_ss();
   getCurrentTimeForExecTriggerRange().setValue(currentTime);
@@ -17,43 +18,53 @@ function executeControl(){
   let isExecOnceBooted = false; // 以下のループで複数の exec を起動しないように制御するためのフラグ
   getExecGroupNames().forEach(
     execGroupName => {
+      console.log("start handle exec group: execGroupName: " + execGroupName);
       // exec が一度起動された場合は後続の exec を全て skip する
       if(isExecOnceBooted){
+        console.log("skip exec group: isExecOnceBooted: execGroupName: " + execGroupName);
         return;
       }
       // 該当 exec group が activate されていない場合は skip する
       const execGrActivatedAtRange = getExecGrActivatedAtRangeFrom(execGroupName);
       if(!isActiveExecOrExecGroup(execGrActivatedAtRange)){
+        console.log("skip exec group: is not activate: execGroupName: " + execGroupName);
         return;
       }
       // 該当 exec group 配下の exec を順に起動する
       const groupExecNames = getGroupExecNamesFrom(execGroupName);
       groupExecNames.forEach(
         execName => {
+          console.log("start handle exec: execName: " + execName);
           // exec が一度起動された場合は後続の exec を全て skip する
           if(isExecOnceBooted){
+            console.log("skip handle exec: isExecOnceBooted: execName: " + execName);
             return;
           }
           // 該当 exec が activate されていない場合は skip する
           const execActivatedAtRange = getExecActivatedAtRangeFrom(execName);
           if(!isActiveExecOrExecGroup(execActivatedAtRange)){
+            console.log("skip handle exec: is not activate: execName: " + execName);
             return;
           }
           // exec 起動済みフラグを立てる
           isExecOnceBooted = true;
           try {
-            // 該当 exec を起動する
+            // 該当 exec を起動する TODO: 各 execute~ でやっている handleError , start/end logging をここに移管する
+            console.log("handle exec: boot exec: execName: " + execName);
             bootExec(execName);
           } catch(error){
             // 異常終了した場合
+            console.log("handle exec: exec failed: execName: " + execName);
             handleExecFailed(execActivatedAtRange, execGrActivatedAtRange, error);
           }
           // 正常終了した場合
           handleExecSucceeded(execActivatedAtRange, groupExecNames, execName, execGrActivatedAtRange);
+          console.log("end handle exec: execName: " + execName);
         }
       );
     }
   );
+  console.log("end executeControl");
 }
 
 // exec group の activate trigger を処理する
