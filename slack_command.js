94be89797335c5840c1f8fecb2951ce187dbe6cb
KenyaSogo <kenyasogo.dev@gmail.com>
2021-10-02 20:22:42 +0900

    fix: slack slash command: bug: access parameter

--- slack_command.js
+++ slack_command.js
@@ -1,11 +1,10 @@
 // slack command からの post リクエストを処理する
 function doPost(e){
-  if(e.command == "/test-pfm"){
+  if(e.parameter.command == "/test-pfm"){
     executeScenarioTest();
     return getSlackCommandPlainResponse("my pfm: execute scenario test was kicked");
   }
 
-  console.log(e);
   return getSlackCommandPlainResponse("my pfm: command not found: " + e.command);
 }
 
