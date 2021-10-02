9964d8a62ca6cac581b4e3d27630a65e138caa7b
KenyaSogo <kenyasogo.dev@gmail.com>
2021-10-02 20:40:05 +0900

    fix: slack slash command: asynchronize

--- slack_command.js
+++ slack_command.js
@@ -1,10 +1,21 @@
 // slack command からの post リクエストを処理する
 function doPost(e){
+  console.log("start: do post");
+  console.log("e: " + e);
+
+  // TODO: token の検証
   if(e.parameter.command == "/test-pfm"){
-    executeScenarioTest();
+    // executeScenarioTest の時間主導型トリガーを発火させる (3秒以内に返さないと slack コマンドがタイムアウトするため非同期実行とする)
+    ScriptApp.newTrigger('executeScenarioTest')
+      .timeBased()
+      .after(1000)
+      .create();
+
+    console.log("end: do post");
     return getSlackCommandPlainResponse("my pfm: execute scenario test was kicked");
   }
 
+  console.log("end: do post");
   return getSlackCommandPlainResponse("my pfm: command not found: " + e.command);
 }
 
