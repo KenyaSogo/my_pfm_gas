4b5ed99606dda90a09970ecc6ea32b0b8c19b056
KenyaSogo <kenyasogo.dev@gmail.com>
2021-10-02 18:55:15 +0900

    fix: slack slash command: treat command not found case

--- slack_command.js
+++ slack_command.js
@@ -2,9 +2,15 @@
 function doPost(e){
   if(e.command == "/test-pfm"){
     executeScenarioTest();
-    const response = {text: "pfm: execute scenario test was kicked"};
-    return ContentService
-      .createTextOutput(JSON.stringify(response))
-      .setMimeType(ContentService.MimeType.JSON);
+    return getSlackCommandPlainResponse("my pfm: execute scenario test was kicked");
   }
+
+  return getSlackCommandPlainResponse("my pfm: command not found: " + e.command);
+}
+
+function getSlackCommandPlainResponse(message){
+  const response = {text: message};
+  return ContentService
+    .createTextOutput(JSON.stringify(response))
+    .setMimeType(ContentService.MimeType.JSON);
 }
