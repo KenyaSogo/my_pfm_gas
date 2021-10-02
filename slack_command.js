b49637c94d0f960108de6fd49656d88c5a936c6e
KenyaSogo <kenyasogo.dev@gmail.com>
2021-10-02 20:57:52 +0900

    fix: slack slash command: a bit refactor

--- slack_command.js
+++ slack_command.js
@@ -5,11 +5,9 @@ function doPost(e){
 
   // TODO: token の検証
   if(e.parameter.command == "/test-pfm"){
-    // executeScenarioTest の時間主導型トリガーを発火させる (3秒以内に返さないと slack コマンドがタイムアウトするため非同期実行とする)
-    ScriptApp.newTrigger('executeScenarioTest')
-      .timeBased()
-      .after(1000)
-      .create();
+    // executeScenarioTest のトリガーを発火させる
+    kickJobExecutionAsynchronously("executeScenarioTest");
+    postConsoleAndSlackInfoMessage("job execution was triggered: slack command, job: /test-pfm, scenario test");
 
     console.log("end: do post");
     return getSlackCommandPlainResponse("my pfm: execute scenario test was kicked");
@@ -19,6 +17,16 @@ function doPost(e){
   return getSlackCommandPlainResponse("my pfm: command not found: " + e.command);
 }
 
+// 関数名で指定された job execution の時間主導型トリガーを発火させる (3秒以内に返さないと slack コマンドがタイムアウトするため非同期実行とする)
+function kickJobExecutionAsynchronously(funcName){
+  ScriptApp.newTrigger(funcName)
+    .timeBased()
+    .after(1000)
+    .create();
+}
+
+
+// slack コマンドへのプレーンなレスポンスを取得する
 function getSlackCommandPlainResponse(message){
   const response = {text: message};
   return ContentService
